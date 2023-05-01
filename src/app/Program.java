7e309d2e48b3f6eac1816396a2ac07c96b351219
bigodom <pedro_augusto50@hotmail.com>
2023-04-30 21:40:51 -0300

    Chess Board

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,12 +1,12 @@
 package app;
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) throws Exception {
         
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
 
     }
 }
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -10,38 +10,21 @@ public class Position {
         this.column = column;
     }
 
-    
 
-    /**
-     * @return int return the row
-     */
     public int getRow() {
-        return row;
+        return this.row;
     }
 
-    /**
-     * @param row the row to set
-     */
     public void setRow(int row) {
         this.row = row;
     }
 
-    /**
-     * @return int return the column
-     */
     public int getColumn() {
-        return column;
+        return this.column;
     }
 
-    /**
-     * @param column the column to set
-     */
     public void setColumn(int column) {
         this.column = column;
     }
-
-    @Override
-    public String toString() {
-        return row + ", " + column;
-    }
+    
 }
